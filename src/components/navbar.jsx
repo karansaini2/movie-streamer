a6afc679e605e79447dab874b9fdd223be5c6ae7
karansaini2 <karansaini0868@gmail.com>
2023-03-19 11:55:29 +0530

    icons added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <section className=" bg-slate-100 h-12 p-2">
-      <nav className="flex justify-evenly items-center space-x-7 sm:space-x-28  ">
+      <nav className="flex justify-evenly items-center space-x-10 sm:space-x-28  ">
         <h2 className="text-2xl font-semibold font-serif xs:text-3xl">
           {" "}
           Streamer
@@ -10,10 +10,18 @@ const Navbar = () => {
 
         <ul className="flex space-x-7  sm:space-x-14">
           <li className="text-lg font-serif p-1 hover:bg-violet-500 hover:text-white rounded-lg ">
-            <Link to="/"> Home</Link>{" "}
+            <Link to="/">
+              {" "}
+              <i className="fa-solid fa-house-user  text-lg xs:text-xl sm:text-2xl sm:hidden"></i>
+              <span className="hidden sm:flex">Home</span>
+            </Link>{" "}
           </li>
           <li className="text-lg font-serif p-1 hover:bg-violet-500 hover:text-white rounded-lg">
-            <Link to="/favourites"> Favourites</Link>
+            <Link to="/favourites">
+              {" "}
+              <i className="fa-solid fa-star  text-lg xs:text-xl sm:text-2xl sm:hidden"></i>{" "}
+              <span className="hidden sm:flex">Favourites</span>
+            </Link>
           </li>
         </ul>
       </nav>
